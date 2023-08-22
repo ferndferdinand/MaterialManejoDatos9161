25fc1a438244c40b6c8657c3be92a2c452977f63
Fernando <ferd.fernando@ciencias.unam.mx>
2023-08-22 07:37:45 -0600

    Modificamos archivo python

--- EjemploPython.py
+++ EjemploPython.py
@@ -3,4 +3,7 @@ print("Este es un ejemplo de código para un proyecto con GIT")
 x = 8
 y = 5
 
-print(f"Resultado de {x} + {y} = {x+y}" )
+print(f"Resultado de {x} + {y} = {x+y}")
+
+print(f"Resultado de {x} - {y} = {x-y}")
+print(f"Resultado de {x} * {y} = {x*y}")
