15f64ecf3fec2f7e14136253a4fb591ab8734ad0
Fernando <ferd.fernando@ciencias.unam.mx>
2023-08-24 07:38:28 -0600

    Se crea función factorial

--- EjemploPython.py
+++ EjemploPython.py
@@ -1,4 +1,4 @@
-print("Este es un ejemplo de c√≥digo para un proyecto con GIT")
+print("Este es un ejemplo de código para un proyecto con GIT")
 
 x = 15
 y = 5
@@ -7,3 +7,12 @@ print(f"Resultado de {x} + {y} = {x+y}")
 
 print(f"Resultado de {x} - {y} = {x-y}")
 print(f"Resultado de {x} / {y} = {x/y}")
+
+
+def factorial(n):
+    fac = 1
+    for i in range(n,1,-1):
+        fac *= i
+    return fac
+
+print(factorial(5))
